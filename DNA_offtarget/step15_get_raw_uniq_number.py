ae28c3ec5e812870c0f54796eb9115e38724d96f
shaoxing Dai <daishx08@gmail.com>
2021-09-28 08:55:43 +0800

    Delete DNA_offtarget directory

--- DNA_offtarget/step15_get_raw_uniq_number.py
+++
@@ -1,7 +0,0 @@
-import os
-fout=open('raw_uniq_number.txt','w')
-samplelist=['07027', '08431GFP', '08431WT', 'NC1', 'NC2', 'NC3', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8','cloning-W','cloning-PF','cloning-JR']
-for i in samplelist:
-	fname1=i+'_4method_raw_dp20_filter_uniq.txt'
-	f1=open(fname1,'r')
-	fout.write(i+'\t'+str(len(f1.readlines()))+'\n')
